5649aaf6b1bc88cb386013af243e60cc7da1d676
Ivan <myemail@yandex.ru>
2018-11-14 00:55:07 +0300

    add WebConfig

---
+++ src/main/java/ru/homelab/config/WebConfig.java
@@ -0,0 +1,12 @@
+package ru.homelab.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"ru.homelab.controller"})
+public class WebConfig implements WebMvcConfigurer {
+}
